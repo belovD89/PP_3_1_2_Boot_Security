15da46c33dd82df18809bf984222da6fa1dd834b
belovD89 <belov-dy@mail.ru>
2024-04-26 10:58:24 +0300

    роль содзан конструктор

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -37,6 +37,10 @@ public class Role implements GrantedAuthority {
         this.name = name;
     }
 
+    public Role(Long id) {
+        this.id = id;
+    }
+
     @Override
     public String getAuthority() {
         return name;
